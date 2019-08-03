eff95989bca914977b2c337827b92382640e5117
Meletis <melnikolaidis86@gmail.com>
2019-08-04 00:14:03 +0300

    Moved shop data, added directory and collection on redux, nested routing for category page

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,7 +4,7 @@ import { createStructuredSelector } from "reselect";
 
 import './collections-overview.styels.scss';
 import CollectionPreview from "../collection-preview/collection-preview.component";
-import { selectCollections } from "../../redux/shop/shop.selectors";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
 const CollectionsOverview = ({ collections }) => (
     <div className='collections-overview'>
@@ -17,7 +17,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapToStateProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 });
 
 export default connect(mapToStateProps)(CollectionsOverview);
