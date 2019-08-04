a1f7b1eeb1fbdb87f56c5259a6ef8df875c95042
Meletis <melnikolaidis86@gmail.com>
2019-08-04 15:37:26 +0300

    updated stripe integration, title, favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_4PY1JiIK6CZH3HvJnTZCcQrz00r96mB0S6';
+
+    const onToken = token => {
+        console.log(token)
+    };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`You total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+};
+
+export default StripeCheckoutButton;
