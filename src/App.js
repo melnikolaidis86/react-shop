f9f5d38845f073e957c00472971ff4a5b11d892f
Meletis <melnikolaidis86@gmail.com>
2019-10-31 00:00:37 +0200

    Added collection data to firebase and fetched them into shop reducer

--- src/App.js
+++ src/App.js
@@ -33,9 +33,9 @@ class App extends React.Component {
                         ...snapshot.data()
                     });
                 });
-            } else {
-                setCurrentUser(userAuth) ;
             }
+
+            setCurrentUser(userAuth);
         });
     }
 
